fbe35e95cb39436cdaddef3f493b2ec82449c194
Narendra Singh <156831315+Narsa011@users.noreply.github.com>
2025-10-18 08:33:00 -0700

    Todo app simple Ui added

---
+++ projact/1-todo-app-version-one/src/App.jsx
@@ -0,0 +1,17 @@
+import AppTodo, { AppTodo2, AppName } from "./assets/camponant/AddTodo";
+
+function App() {
+  return (
+    <>
+      <AppName />
+      <br />
+      <AppTodo />
+      <br />
+      <AppTodo2 />
+      <br />
+      <AppTodo2 />
+    </>
+  );
+}
+
+export default App;
