09927b64dabfdec42a259647b825cb6c85f6bbd4
Narendra Singh <156831315+Narsa011@users.noreply.github.com>
2025-10-23 01:44:25 -0700

    Complete Calulatore projact

---
+++ projact/Calculator-VersonOne/Calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import Display from "./camponants/Display";
+import styles from "./App.module.css";
+import ButtonsContainer from "./camponants/ButtonsContainer";
+
+function App() {
+  return (
+    <div className={styles.calculator}>
+      <Display />
+      <ButtonsContainer />
+    </div>
+  );
+}
+export default App;
